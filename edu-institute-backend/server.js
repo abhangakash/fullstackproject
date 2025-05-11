fb0d32d195e8a0da3169b88936cac8547088eb8e
abhangakash <iakashabhang@gmail.com>
2025-05-11 23:29:45 +0530

    Fix bcrypt installation issue

--- edu-institute-backend/server.js
+++ edu-institute-backend/server.js
@@ -53,9 +53,10 @@ app.use('/api', uploadRoute);
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log('MongoDB connection error:', error));
+
 
 // Start the server
 const PORT = process.env.PORT || 5000;
