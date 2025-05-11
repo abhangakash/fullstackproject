0844edb3b562069f911fd79462e586afa356dd7c
abhangakash <iakashabhang@gmail.com>
2025-05-11 22:44:04 +0530

    Add Contact model

--- edu-institute-backend/routes/gallery.js
+++ edu-institute-backend/routes/gallery.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Gallery = require('../models/Gallery');
+const Gallery = require('../models/gallery');
 
 // GET all gallery images
 router.get('/', async (req, res) => {
