fef5ae05d40f2186159519bc0a1ad6d9fe869003
abhangakash <iakashabhang@gmail.com>
2025-05-16 19:56:07 +0530

    Chatbot Integration

--- edu-institute-backend/server.js
+++ edu-institute-backend/server.js
@@ -15,8 +15,8 @@ const app = express();
 
 // ✅ Allow specific frontend origins
 
-const allowedOrigins = [,
-  ,
+const allowedOrigins = ['https://www.abhang.site',
+  'https://edu-backend-cz0l.onrender.com',
   'http://localhost:3000']; // Add Vercel/Render if needed
 
 app.use(cors({
